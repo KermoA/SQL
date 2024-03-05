33c44784d3db075214c5a6e92bbc6a56ecd5933b
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-03-05 12:15:45 +0200

    TARgeDatabase

--- Query.sql
+++ Query.sql
@@ -157,3 +157,161 @@ select * from Person order by cast(Age as int)
 
 --kõikide isikute koondvanus
 select sum(cast(Age as int)) from Person
+
+
+--kuvab kõige nooremat isikut
+select min(cast(Age as int)) from Person
+--kuvab kõige vanemat isikut
+select max/cast(Age as int)) from Person
+
+--näeme konkreetsetes linnades olevate isikute koondvanust
+select City, sum(cast(Age as int)) as TotalAge from Person
+group by City
+
+--kuidas saab koodiba muuta andmetüüpi ja selle pikkust
+alter table Person
+alter column Age int
+
+--kuvab esimeses reas väljatoodud järjestuses ja muudab Age-i TotalAge-ks
+--järjestab Citys olevate nimede järgi ja siis GenderId järgi
+select City, GenderId, sum(Age) as TotalAge from Person
+group by City, GenderId
+order by City
+
+insert into Person values
+(11, 'Robin', 'robin@r.com', 1, 29, 'Gotham')
+
+--näitab rirade arvu tabelis
+select count(*) from Person
+select * from Person
+
+--näitab tulemust, et mitu inimest on GenderId väärtusega 2 konkreetses linnas
+--arvutab vanuse kokku selles linnas
+select GenderId, City, sum(Age) as TotalAge, count(Id) as [Total Person(s)]
+from Person
+where GenderId = '1'
+group by GenderId, City
+
+--näitab ära inimeste koondvanuse, mis on üle 41 aasta ja
+--kui palju neid igas linnas elab 
+--eristab inimese soo ära
+select GenderId, City, sum(Age) as TotalAge, count(Id)
+as [Total Person(s)]
+from Person
+group by GenderId, City having sum(Age) > 41
+
+--loome tabelid Employees ja department
+
+create table Department
+(
+Id int not null primary key,
+DepartmentName nvarchar(50),
+Location nvarchar(50),
+DepartmentHead nvarchar(50)
+)
+
+create table Employees
+(
+Id int not null primary key,
+Name nvarchar(50),
+Gender nvarchar(50),
+Salary nvarchar(50),
+DepartmentId int
+)
+
+insert into Employees (Id, Name, Gender, Salary, DepartmentId) values
+(1, 'Tom', 'Male', 4000, 1),
+(2, 'Pam', 'Female', 3000, 3),
+(3, 'John', 'Male', 3500, 1),
+(4, 'Sam', 'Male', 4500, 2),
+(5, 'Todd', 'Male', 2800, 2),
+(6, 'Ben', 'Male', 7000, 1),
+(7, 'Sara', 'Female', 4800, 3),
+(8, 'Valarie', 'Female', 5500, 1),
+(9, 'James', 'Male', 6500, NULL),
+(10, 'Russell', 'Male', 8800, NULL)
+
+insert into Department (Id, DepartmentName, Location, DepartmentHead) 
+values
+(1, 'IT', 'London', 'Rick'),
+(2, 'Payroll', 'Delhi', 'Ron'),
+(3, 'HR', 'New York', 'Christie'),
+(4, 'Other Department', 'Sydney', 'Cindrella')
+
+select * from Department
+select * from Employees
+
+--left join
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+
+--arvutab kõikide palgad kokku
+select sum(cast(Salary as int)) as TotalSalary from Employees
+--min palga saaja
+select min(cast(Salary as int)) As MinSalary from Employees
+--ühe kuu palga saaja linnade lõikes
+select Location, sum(cast(Salary as int)) as TotalSalary
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+group by Location
+
+alter table Employees
+add City nvarchar(30)
+
+select * from Employees
+update Employees
+set City = 'New York'
+where Id = 10
+
+--näitab erinevust palgafondi osas nii linnade kui ka soo osas
+select City, Gender, sum(cast(Salary as int)) as TotalSalary from Employees
+group by City, Gender
+--sama päring nagu eelmine, aga linnad on tähestikulises järjestuses
+select City, Gender, sum(cast(Salary as int)) as TotalSalary from Employees
+group by City, Gender
+order by City
+
+--loeb ära ridade arvu Employees tabelis
+select count(*) as [Ridade arv] from Employees
+
+--mitu töötajat on soo ja linna kaupa
+select Gender, City,
+count (Id) as [Total Employees]
+from Employees
+group by Gender, City
+
+--kuvab ainult kõik naised linnade kaupa
+select Gender, City,
+count (Id) as [Total Employees]
+from Employees
+where Gender = 'Female'
+group by Gender, City
+
+--kuvab ainult kõik mehed linnade kaupa
+--ja kasutame having
+select Gender, City,
+count (Id) as [Total Employees]
+from Employees
+group by Gender, City
+having Gender = 'Male'
+
+select * from Employees where  sum(cast(Salary as int)) > 4000
+
+select Gender, City, sum(cast(Salary as int)) as TotalSalary, count (Id)
+as [Total Employee(s)]
+from Employees group by Gender, City
+having sum(cast(Salary as int)) > 4000
+
+--loome tabeli, milles hakatakse automaatselt nummerdama Id-d
+create table Test1
+(
+Id int identity(1,1),
+Value nvarchar(20)
+)
+
+insert into Test1 values ('X')
+select * from Test1
+
